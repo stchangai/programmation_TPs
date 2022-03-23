bed638aca0eed287e66e2472605dc337c557fb0d
Tchangai <solem.tchangai@orange.fr>
2022-03-23 15:36:20 +0100

    [Update] Refactoring Hangman exo

--- Simple-Cpp-Setup-main/src/Hangman/playHangman.cpp
+++ Simple-Cpp-Setup-main/src/Hangman/playHangman.cpp
@@ -4,43 +4,61 @@
 #include "getLetterFromUser.hpp"
 #include "getWordToGuess.hpp"
 
+void FillResultWithDashAtBeginning(std::string& result, std::string wordToGuess)
+{
+    for (size_t i = 0; i < wordToGuess.size(); i++) {
+        result.push_back('_');
+    }
+}
+
+bool ReplaceLetterInWord(size_t& findLetterPos, char letterGivedByUser, std::string& result, int& letterFoundCount, std::string wordToGuess)
+{
+    bool isALetterFound = false;
+    while (findLetterPos != std::string::npos) {
+        result[findLetterPos] = letterGivedByUser;
+        letterFoundCount--;
+        findLetterPos  = wordToGuess.find(letterGivedByUser, findLetterPos + 1);
+        isALetterFound = true;
+    }
+    return isALetterFound;
+}
+
+void DisplayResultEndGame(int lives, std::string wordToGuess, std::string result)
+{
+    if (lives == 0) {
+        std::cout << "you lose, sorry ! The word was " << wordToGuess << std::endl;
+    }
+    else {
+        std::cout << "Wouah you are a real killer gamer ! You win ! The word is indeed " << result << std::endl;
+    }
+}
+
 void playHangman()
 {
     std::string wordToGuess       = getWordToGuess();
     std::string letterGivedByUser = " ";
     std::string result;
     int         letterFoundCount = static_cast<int>(wordToGuess.length());
-    for (size_t i = 0; i < wordToGuess.size(); i++) {
-        result.push_back('_');
-    }
+
+    FillResultWithDashAtBeginning(result, wordToGuess);
     std::cout << wordToGuess << std::endl;
     std::string lettersStocked;
-    int         lives          = 8;
-    bool        isALetterFound = false;
+    int         lives = 8;
+    bool        isALetterFound;
     while (lives > 0 && letterFoundCount != 0) {
         std::cout << "You have " << lives << " lives" << std::endl;
-        isALetterFound = false;
+
         std::cout << result << std::endl;
         letterGivedByUser[0] = getLetterFromUser();
 
         size_t findLetterPos = wordToGuess.find(letterGivedByUser[0]);
-        std::cout << "findLetterPos = " << findLetterPos << std::endl;
-        while (findLetterPos != std::string::npos) {
-            std::cout << "boucle while entrée " << std::endl;
-            result[findLetterPos] = letterGivedByUser[0];
-            isALetterFound        = true;
-            letterFoundCount--;
-            findLetterPos = wordToGuess.find(letterGivedByUser[0], findLetterPos + 1);
-        }
 
-        if (isALetterFound == false) {
+        isALetterFound = ReplaceLetterInWord(findLetterPos, letterGivedByUser[0], result, letterFoundCount, wordToGuess);
+
+        if (!isALetterFound) {
             lives--;
         }
     }
-    if (lives == 0) {
-        std::cout << "you lose, sorry ! The word was " << wordToGuess << std::endl;
-    }
-    else {
-        std::cout << "Wouah you are a real killer gamer ! You win ! The word is indeed" << result << std::endl;
-    }
+
+    DisplayResultEndGame(lives, wordToGuess, result);
 }
