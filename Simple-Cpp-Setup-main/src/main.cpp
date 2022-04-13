416edad55365c3f50bb80e0561502cc13cd8659b
Tchangai <solem.tchangai@orange.fr>
2022-04-13 15:30:16 +0200

    [Add] Installation p6 - window  test displaying OK

--- Simple-Cpp-Setup-main/src/main.cpp
+++ Simple-Cpp-Setup-main/src/main.cpp
@@ -1,3 +1,4 @@
+#include <p6/p6.h>
 #include <iostream>
 #include <random>
 #include "./Hangman/playHangman.hpp"
@@ -8,11 +9,18 @@
 
 int main()
 {
-    bool quit = false;
-    while (!quit) {
-        showListOfCommands();
-        const auto command = getInputFromUser();
-        quit               = CheckValueOfInput(static_cast<Command>(command));
-    }
+    auto ctx = p6::Context{{1280, 720, "Hello p6"}};
+    // ctx.update = [&]() {
+    //     ctx.background({0.5f, 0.3f, 0.8f});
+    //     ctx.circle(p6::Center{ctx.mouse()} p6::Radius{0.3f});
+    // };
+    ctx.start();
+    /********** GAMES HANGMAN - GUESSAWORD *************/
+    // bool quit = false;
+    // while (!quit) {
+    //     showListOfCommands();
+    //     const auto command = getInputFromUser();
+    //     quit               = CheckValueOfInput(static_cast<Command>(command));
+    // }
     return 0;
 }
